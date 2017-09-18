d3a0226d5bcd2bd9cb0bf6c249cb62fc4c323cb8
Modulus <thejohnskauge@gmail.com>
2017-09-18 23:16:58 +0200

    Added more tests

--- tests/test_postgres.py
+++ tests/test_postgres.py
@@ -1,5 +1,19 @@
 def test_postgres_service(host):
     postgres = host.service("postgresql")
 
-    #assert postgres.is_running
-    #assert postgres.is_enabled
+    assert postgres.is_running
+    assert postgres.is_enabled
+
+def test_postgres_port_and_localhost(host):
+    socket = host.socket("tcp://127.0.0.1:5433")
+
+    assert socket.is_listening
+
+
+def test_postgresql_user(host):
+    passwd = host.file("/etc/passwd")
+
+    assert passwd.is_file
+    assert passwd.user == "root"
+    assert passwd.group == "root"
+    assert passwd.contains("postgres:x")
